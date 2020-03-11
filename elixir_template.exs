9c24c49b73df629ba1fb3efe39e9da1848e7505e
W Millon <wjmillon@gmail.com>
2020-03-11 06:30:41 -0500

    added simple elixir
    added template for future elixir challenges

---
+++ elixir_template.exs
@@ -0,0 +1,35 @@
+defmodule Solution do
+  def exec() do
+    read_input(nil)
+  end
+
+  defp read_input(:float),
+    do:
+      IO.gets("") |> parse_input(:float)
+
+  defp read_input(:int),
+    do:
+      IO.gets("") |> parse_input(:int)
+
+  defp read_input(_),
+    do:
+      IO.gets("") |> parse_input(nil)
+
+  defp parse_input(input, :float),
+    do:
+      input
+      |> String.trim()
+      |> String.to_float()
+
+  defp parse_input(input, :int),
+    do:
+      input
+      |> String.trim()
+      |> String.to_integer()
+
+      defp parse_input(input, _), do: input |> String.trim()
+
+  defp write_output(n), do: IO.puts(n)
+end
+
+Solution.exec()
